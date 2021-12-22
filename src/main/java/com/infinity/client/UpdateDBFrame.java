f571243df974f544dd3f9057de74eda1337ce65a
hoangnv <viethoang.ma1009@gmail.com>
2021-12-22 08:32:15 +0700

    fix update info

--- src/main/java/com/infinity/client/UpdateDBFrame.java
+++ src/main/java/com/infinity/client/UpdateDBFrame.java
@@ -41,7 +41,7 @@ public class UpdateDBFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public UpdateDBFrame(ClientController clientController, FileServerController fileServerController, 
+	public UpdateDBFrame(ClientController clientController, FileServerController fileServerController,
 			FileReceiverController fileReceiverController, String username, String sharedFolderName, Logger LOGGER) {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 671, 497);
@@ -49,55 +49,42 @@ public class UpdateDBFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblSharedFiles = new JLabel("Shared Files");
 		lblSharedFiles.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSharedFiles.setFont(new Font("UD Digi Kyokasho NK-B", Font.PLAIN, 20));
 		lblSharedFiles.setBounds(233, 22, 184, 36);
 		contentPane.add(lblSharedFiles);
-		
+
 		JLabel notificationText = new JLabel("");
 		notificationText.setForeground(Color.RED);
 		notificationText.setHorizontalAlignment(SwingConstants.CENTER);
 		notificationText.setFont(new Font("UD Digi Kyokasho NK-B", Font.PLAIN, 15));
 		notificationText.setBounds(130, 420, 400, 25);
 		contentPane.add(notificationText);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(23, 94, 610, 310);
 		contentPane.add(scrollPane);
-		
+
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-			},
-			new String[] {
-				"File Name", "Checksum", "Size (Byte)"
-			}
-		) {
+				new Object[][] { { null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, },
+				new String[] { "File Name", "Checksum", "Size (Byte)" }) {
 			/**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
-			Class[] columnTypes = new Class[] {
-				String.class, Long.class, Long.class
-			};
+			Class[] columnTypes = new Class[] { String.class, Long.class, Long.class };
+
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
-			boolean[] columnEditables = new boolean[] {
-				false, false, false
-			};
+
+			boolean[] columnEditables = new boolean[] { false, false, false };
+
 			public boolean isCellEditable(int row, int column) {
 				return columnEditables[column];
 			}
@@ -107,88 +94,91 @@ public class UpdateDBFrame extends JFrame {
 		table.getColumnModel().getColumn(2).setResizable(false);
 		table.setFont(new Font("UD Digi Kyokasho NK-B", Font.PLAIN, 14));
 		scrollPane.setViewportView(table);
-		
+
 		JTableHeader tableHeader = table.getTableHeader();
 		tableHeader.setOpaque(false);
 		tableHeader.setBackground(new Color(210, 210, 210));
 		tableHeader.setFont(new Font("UD Digi Kyokasho NK-B", Font.BOLD, 14));
 		table.setRowHeight(28);
-		
+
 		displayDataInTable(table, FileServerController.getSharedFiles());
-		
+
 		List<SharedFileModel> actualSharedFilesModel = FileServerController.getFilesInSharedFolder(sharedFolderName);
 		List<Long> actualSharedFile = new ArrayList<Long>();
 		for (SharedFileModel file : actualSharedFilesModel) {
 			file.setSharer(username);
 			actualSharedFile.add(file.getChecksum());
 		}
-		
+
 		List<Long> currentDBSharedFiles = new ArrayList<Long>();
 		for (Long key : FileServerController.getSharedFiles().keySet()) {
-		    currentDBSharedFiles.add(key);
+			currentDBSharedFiles.add(key);
 		}
-		
+
 		int numActualSharedFiles = actualSharedFile.size();
 		int numCurrentDBSharedFiles = currentDBSharedFiles.size();
-		
-		if (numActualSharedFiles != numCurrentDBSharedFiles) {
-			notificationText.setText("You need to update your files information !");
-		}
-		
+
+		notificationText.setText("Please update if you have changed the shared file !");
+//		if (numActualSharedFiles != numCurrentDBSharedFiles) {
+//			
+//		}
+
 		JButton btnUpdate = new JButton("Update");
 		btnUpdate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				List<SharedFileModel> payload = new ArrayList<SharedFileModel>();
-				
-				if (numActualSharedFiles > numCurrentDBSharedFiles) {
-					
-					for (SharedFileModel sharedFile : actualSharedFilesModel) {
-						if (!fileServerController.contains(sharedFile.getChecksum())) {
-							payload.add(sharedFile);
-						}
+
+				List<SharedFileModel> payloadPublish = new ArrayList<SharedFileModel>();
+				List<SharedFileModel> payloadUnPublish = new ArrayList<SharedFileModel>();
+
+				for (SharedFileModel sharedFile : actualSharedFilesModel) {
+					if (!fileServerController.contains(sharedFile.getChecksum())) {
+						payloadPublish.add(sharedFile);
 					}
-					
-					if ( clientController.shareFile(payload) ) {
-						for (SharedFileModel sharedFile : payload) {
+				}
+
+				if (payloadPublish.size() > 0) {
+					if (clientController.shareFile(payloadPublish)) {
+						for (SharedFileModel sharedFile : payloadPublish) {
 							FileServerController.shareNewFile(sharedFile.getChecksum(), sharedFile);
 						}
 						notificationText.setText("");
-						LOGGER.info("File shared: " + JSON.toJSONString(payload));
+						LOGGER.info("File shared: " + JSON.toJSONString(payloadPublish));
 					} else {
-						JOptionPane.showMessageDialog(null, "Something wrong with your network\n or there's a file have the same checksum. !!!");
+						JOptionPane.showMessageDialog(null,
+								"Something wrong with your network\n or there's a file have the same checksum. !!!");
 					}
-					
-				} else if (numActualSharedFiles < numCurrentDBSharedFiles) {
-					
-					for (Long cs : currentDBSharedFiles) {
-						if (!actualSharedFile.contains(cs)) {
-							payload.add(FileServerController.getSharedFiles().get(cs));
-						}
+				}
+
+				for (Long cs : currentDBSharedFiles) {
+					if (!actualSharedFile.contains(cs)) {
+						payloadUnPublish.add(FileServerController.getSharedFiles().get(cs));
 					}
-					
-					if ( clientController.unshareFile(payload) ) {
-						for (SharedFileModel unsharedFile : payload) {
+				}
+				System.out.println(payloadUnPublish.size());
+				if (payloadUnPublish.size() > 0) {
+					if (clientController.unshareFile(payloadUnPublish)) {
+						for (SharedFileModel unsharedFile : payloadUnPublish) {
 							fileServerController.unshareFile(unsharedFile.getChecksum());
 						}
 						notificationText.setText("");
-						LOGGER.info("File unshared: " + JSON.toJSONString(payload));
+						LOGGER.info("File unshared: " + JSON.toJSONString(payloadUnPublish));
 					} else {
 						JOptionPane.showMessageDialog(null, "Something wrong with your network !!!");
 					}
-					
-				} else {
-					JOptionPane.showMessageDialog(null, "Your files are up to date",
-							"Updater", JOptionPane.INFORMATION_MESSAGE);
 				}
-				
+
+				if (payloadPublish.size() == 0 && payloadUnPublish.size() == 0) {
+					JOptionPane.showMessageDialog(null, "Your files are up to date", "Updater",
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+
 				displayDataInTable(table, FileServerController.getSharedFiles());
 			}
 		});
 		btnUpdate.setFont(new Font("UD Digi Kyokasho NK-B", Font.BOLD, 15));
 		btnUpdate.setBounds(23, 22, 130, 30);
 		contentPane.add(btnUpdate);
-		
+
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -199,17 +189,17 @@ public class UpdateDBFrame extends JFrame {
 		btnCancel.setFont(new Font("UD Digi Kyokasho NK-B", Font.BOLD, 15));
 		btnCancel.setBounds(498, 22, 130, 30);
 		contentPane.add(btnCancel);
-		
+
 	}
-	
+
 	private void displayDataInTable(JTable table, Map<Long, SharedFileModel> map) {
 		Application.clearDataTable(table);
 		int count = 0;
 		for (SharedFileModel file : map.values()) {
-				table.getModel().setValueAt(file.getFileName(), count, 0);
-				table.getModel().setValueAt(file.getChecksum(), count, 1);
-				table.getModel().setValueAt(file.getSize(), count, 2);
-				count++;
+			table.getModel().setValueAt(file.getFileName(), count, 0);
+			table.getModel().setValueAt(file.getChecksum(), count, 1);
+			table.getModel().setValueAt(file.getSize(), count, 2);
+			count++;
 		}
 	}
 }
